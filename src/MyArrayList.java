515ea7f9403c3898bc752c4f372f9732f95990a6
zbekxz <forestnatplay@yandex.ru>
2023-04-19 23:30:27 +0600

    add additional documentation to MyArrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,6 @@
 public class MyArrayList<T> implements MyList {
-    private T[] arr;
-    private int size;
+    private T[] arr; // array with generic type that can be store any types of value
+    private int size; // size of array
 
     /**
      * @function MyArrayList - constructor
@@ -8,8 +8,8 @@ public class MyArrayList<T> implements MyList {
      * @return void
      */
     public MyArrayList() {
-        this.arr = (T[]) new Object[5];
-        this.size = 0;
+        this.arr = (T[]) new Object[5]; // creating array with allocated space 5
+        this.size = 0; // equate to 0 because of empty of array
     }
     /**
      * @function printList - displaying list in console
@@ -29,9 +29,9 @@ public class MyArrayList<T> implements MyList {
      * @return void
      */
     private void increaseLength() {
-        T[] newArr = (T[]) new Object[arr.length*2];
+        T[] newArr = (T[]) new Object[arr.length*2]; // doubling allocated space of array  to contain more values
         for (int i = 0; i < arr.length; i++) {
-            newArr[i] = arr[i];
+            newArr[i] = arr[i]; // save exist values
         }
         arr = newArr;
     }
@@ -55,7 +55,7 @@ public class MyArrayList<T> implements MyList {
     public boolean contains(Object o) {
         boolean bool = false;
         for (int i = 0; i < size; i++) {
-            if (arr[i] == o) {
+            if (arr[i] == o) { // checking is given element equal to elements of array
                 bool = true;
             }
         }
@@ -69,8 +69,8 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public void add(Object item) {
-        if(size == arr.length){
-            increaseLength();
+        if(size == arr.length){ // checking is array have enough space to add new value
+            increaseLength(); // increase space of array if not
         }
         arr[size++] = (T) item;
     }
@@ -83,10 +83,10 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public void add(Object item, int index) {
-        if (size == arr.length) {
-            increaseLength();
+        if (size == arr.length) { // checking is array have enough space to add new value
+            increaseLength(); // increase space of array if not
         }
-        T[] newArr = (T[]) new Object[arr.length];
+        T[] newArr = (T[]) new Object[arr.length]; // creating new array to swap elements to add new between them
         for (int i = 0; i < index; i++) {
             newArr[i] = arr[i];
         }
@@ -107,7 +107,7 @@ public class MyArrayList<T> implements MyList {
     public boolean remove(Object item) {
         boolean removed = false;
         for (int i = 0; i < size; i++) {
-            if (arr[i] == item) {
+            if (arr[i] == item) { // checking if given item contains in array and remove it after
                 remove(i);
                 removed = true;
             }
@@ -122,10 +122,10 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public Object remove(int index) {
-        checkIndex(index);
-        T removed = arr[index];
+        checkIndex(index); // checking if given index acceptable
+        T removed = arr[index]; // store removed item to return after
         for(int i = index + 1; i < size; i++) {
-            arr[i-1] = arr[i];
+            arr[i-1] = arr[i]; // swap elements to remove element
         }
         size--;
         return removed;
@@ -149,7 +149,7 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public T get(int index) {
-        checkIndex(index);
+        checkIndex(index); // checking if given index acceptable
         return arr[index];
     }
 
@@ -161,11 +161,11 @@ public class MyArrayList<T> implements MyList {
     @Override
     public int indexOf(Object o) {
         for (int i = 0; i < size; i++) {
-            if (arr[i] == o) {
+            if (arr[i] == o) { // checking if given element contains in array
                 return i;
             }
         }
-        return -1;
+        return -1; // returning -1 if element doesn't exist in array
     }
 
     /**
@@ -175,12 +175,12 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public int lastIndexOf(Object o) {
-        for (int i = size - 1; i >= 0; i--) {
-            if (arr[i] == o) {
+        for (int i = size - 1; i >= 0; i--) { // starting from end to find last index
+            if (arr[i] == o) { // checking if given element contains in array
                 return i;
             }
         }
-        return -1;
+        return -1; // returning -1 if element doesn't exist in array
     }
 
     /**
@@ -190,8 +190,9 @@ public class MyArrayList<T> implements MyList {
      */
     @Override
     public void sort() {
-        if (isSortable().equals("int")) {
+        if (isSortable().equals("int")) { // sorting if array have all integers
             int n = size;
+            // bubble sort
             for (int i = 0; i < n-1; i++)
                 for (int j = 0; j < n-i-1; j++)
                     if ((int) arr[j] > (int) arr[j+1])
@@ -202,8 +203,9 @@ public class MyArrayList<T> implements MyList {
                         arr[j+1] = temp;
                     }
         }
-        if (isSortable().equals("double")) {
+        if (isSortable().equals("double")) { // sorting if array have all doubles
             int n = size;
+            // bubble sort
             for (int i = 0; i < n-1; i++)
                 for (int j = 0; j < n-i-1; j++)
                     if ((Double) arr[j] > (Double) arr[j+1])
@@ -222,8 +224,8 @@ public class MyArrayList<T> implements MyList {
      * @return void
      */
     public void checkIndex(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
+        if (index < 0 || index >= size) { // checking if index acceptable
+            throw new IndexOutOfBoundsException(); // throw error if not
         }
     }
 
@@ -253,6 +255,6 @@ public class MyArrayList<T> implements MyList {
         if (doubleSize == size) {
             return "double";
         }
-        return "0";
+        return "0"; // returning 0 if array not acceptable
     }
 }
